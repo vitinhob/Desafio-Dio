19ba02635b320e0efa042043ace0e2a26198c57d
vitinhob <vitinhobordin@gmail.com>
2023-11-23 16:00:32 -0300

    arualizando

--- Teste.js
+++ Teste.js
@@ -1,43 +1,55 @@
-console.log("**Digite seu nome de herói**")
-let nomeHeroi = prompt()
+const entrada = require ("prompt-sync")({sigint: true});
 
-console.log("Seja bem vindo " + nomeHeroi)
+let nomeHeroi;
 
-console.log("Agora digite o nível de experiência do seu herói: ")
-let xp = prompt()
+nomeHeroi = entrada("Qual o nome do seu persongem: ")
+console.log("Seja bem vindo " + nomeHeroi + "\n")
+
+console.log("Se você tiver menos de 1000 de xp Digite 1")
+console.log("Se você tiver entre 1001 e 2000 de xp Digite 2")
+console.log("Se você tiver entre 2001 e 5000 de xp Digite 3")
+console.log("Se você tiver entre 6001 e 7000 de xp Digite 4")
+console.log("Se você tiver entre 7001 e 8000 de xp Digite 5")
+console.log("Se você tiver entre 8001 e 9000 de xp Digite 6")
+console.log("Se você tiver entre 9001 e 10000 de xp Digite 7")
+console.log("Se você tiver mais de 10001 de xp Digite 8\n")
+
+let xp;
+xp = entrada("Agora digite o nível do seu herói: ")
+console.log("")
 
 switch (xp)
 {
-    case 0 <= 1000:
-    console.log(nomeHeroi + "Está no nível Ferro")
+    case "1":
+    console.log(nomeHeroi + " Está no nível Ferro")
     break
 
-    case 1001 <= 2000:
-    console.log(nomeHeroi + "Está no nível Bronze")
+    case "2":
+    console.log(nomeHeroi + " Está no nível Bronze")
     break
 
-    case 2001 <= 5000:
-    console.log(nomeHeroi + "Está no nível Prata")
+    case "3":
+    console.log(nomeHeroi + " Está no nível Prata")
     break
 
-    case 5001 <= 6000:
-    console.log(nomeHeroi + "Está no nível Ouro")
+    case "4":
+    console.log(nomeHeroi + " Está no nível Ouro")
     break
 
-    case 6001 <= 7000:
-    console.log(nomeHeroi + "Está no nível Platina")
+    case "5":
+    console.log(nomeHeroi + " Está no nível Platina")
     break
 
-    case 7001 <= 8000:
-    console.log(nomeHeroi + "Está no nível Ascendente")
+    case "6":
+    console.log(nomeHeroi + " Está no nível Ascendente")
     break
 
-    case 8001 <= 9000:
-    console.log(nomeHeroi + "Está no nível Imortal")
+    case "7":
+    console.log(nomeHeroi + " Está no nível Imortal")
     break
 
-    case 9001 <= 10000:
-    console.log(nomeHeroi + "Está no nível Radiante")
+    case "8":
+    console.log(nomeHeroi + " Está no nível Radiante")
     break
 }
 
